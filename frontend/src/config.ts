ef51c69e80e2f8405c09057a960067b59b7c932f
adriantan09 <64292824+adriantan09@users.noreply.github.com>
2022-09-02 14:14:05 +1000

    Completed Vue Frontend Assessment with all features and extensions (#1)

---
+++ frontend/src/config.ts
@@ -0,0 +1,5 @@
+const BACKEND_PORT = 3000
+export const BACKEND_URL = `http://localhost:${BACKEND_PORT}`
+
+const FRONTEND_PORT = 5173
+export const FRONTEND_URL = `http://localhost:${FRONTEND_PORT}`
